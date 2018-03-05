3efe85e6f0f54378dd639a0898343128939fc431
Alex Mihailov <avmikhaylov@tectus-it.ru>
2018-03-05 22:30:34 +0300

    Добавил таймер

--- app/src/main/java/ghh/auus/fhhh/com/MainActivity.java
+++ app/src/main/java/ghh/auus/fhhh/com/MainActivity.java
@@ -1,5 +1,6 @@
 package ghh.auus.fhhh.com;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
@@ -7,23 +8,34 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
+import cn.iwgang.countdownview.CountdownView;
 
 
 public class MainActivity extends AppCompatActivity implements IView {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private static final long TIME_CLOCK_MILLIS = 5 * 60 * 1000;
+
     private static class PresenterHolder {
         static final PresenterMain INSTANCE = new PresenterMain();
     }
 
+    private View mLayoutTimer;
+    private View mLayoutWeb;
+    private ImageView mButtonStart;
     private WebView webView;
     private ProgressBar progressBar;
+    private CountdownView mClockView;
+
     private PresenterMain mPresenter;
 
     @Override
@@ -32,16 +44,50 @@ public class MainActivity extends AppCompatActivity implements IView {
         Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
 
+        initViews();
+
+        mLayoutTimer.setVisibility(View.VISIBLE);
+        mLayoutWeb.setVisibility(View.GONE);
+
+        setupAnimation();
+
+        setupListener();
+
+        mClockView.start(TIME_CLOCK_MILLIS); // Millisecond
+
+        setupTtti(savedInstanceState);
+    }
+
+    private void initViews() {
         progressBar = findViewById(R.id.progress);
         webView = findViewById(R.id.web_view);
+        mLayoutTimer = findViewById(R.id.layout_timer);
+        mLayoutWeb = findViewById(R.id.layout_web_view);
+        mButtonStart = findViewById(R.id.button_start);
+        mClockView = findViewById(R.id.clock);
+    }
 
+    private void setupListener() {
+        mLayoutTimer.setOnClickListener(view -> {
+            mLayoutTimer.setVisibility(View.GONE);
+            mLayoutWeb.setVisibility(View.VISIBLE);
+
+            mPresenter.showWebView(
+                    getString(R.string.opening_url),
+                    getString(R.string.key_redirect)
+            );
+        });
+    }
+
+    private void setupAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.pulse);
+        animation.setRepeatCount(ObjectAnimator.INFINITE);
+        mButtonStart.setAnimation(animation);
+    }
+    private void setupTtti(Bundle savedInstanceState) {
         mPresenter = PresenterHolder.INSTANCE;
         mPresenter.setView(this);
         mPresenter.onCreateView(savedInstanceState);
-        mPresenter.showWebView(
-                getString(R.string.opening_url),
-                getString(R.string.key_redirect)
-        );
     }
 
     @Override
