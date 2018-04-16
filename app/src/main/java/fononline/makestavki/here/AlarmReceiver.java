d0577ca234facbae6ef31d81773b1939865e11e2
Alex Mihailov <avmikhaylov@tectus-it.ru>
2018-04-16 20:29:52 +0300

    Добавил пуши

---
+++ app/src/main/java/fononline/makestavki/here/AlarmReceiver.java
@@ -0,0 +1,24 @@
+package fononline.makestavki.here;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() != null && context != null) {
+            if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
+                NotificationScheduler.setReminder(context, AlarmReceiver.class,
+                        ConstantTime.hour, ConstantTime.minute);
+                return;
+            }
+        }
+
+        NotificationScheduler.showNotification(context, SplashScreenActvity.class,
+                "Новые МАТЧИ уже тут⏱",
+                "Высокие коэффициенты, быстрые выплаты по новым матчам!");
+
+    }
+}
---
+++ app/src/main/java/fononline/makestavki/here/ConstantTime.java
@@ -0,0 +1,7 @@
+package fononline.makestavki.here;
+
+public class ConstantTime {
+    static final int hour = 20;
+    static final int minute = 0;
+    static final String CHANAL_ID = "CHANAL_ID";
+}
