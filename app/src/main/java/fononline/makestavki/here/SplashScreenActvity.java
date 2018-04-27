165370436c8cab1889385ae6a96d4e18e27b2f96
Alex Mihailov <avmikhaylov@tectus-it.ru>
2018-04-27 20:18:16 +0300

    Добавил пермишены и поднял версию

--- app/src/main/java/fononline/makestavki/here/SplashScreenActvity.java
+++ app/src/main/java/fononline/makestavki/here/SplashScreenActvity.java
@@ -1,13 +1,18 @@
 package fononline.makestavki.here;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 
 public class SplashScreenActvity extends AppCompatActivity {
@@ -18,8 +23,9 @@ public class SplashScreenActvity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         NotificationScheduler.setReminder(SplashScreenActvity.this, AlarmReceiver.class,
                 ConstantTime.hour, ConstantTime.minute);
-        Observable.timer(1, TimeUnit.SECONDS)
-                .subscribe(t -> openOpeningLaunchScreen());
+        Observable.timer(2, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(t -> showDialog());
     }
 
     private void openOpeningLaunchScreen() {
@@ -27,4 +33,33 @@ public class SplashScreenActvity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SplashScreenActvity.this);
+        builder.setTitle("Разрешения для приложения")
+                .setMessage("Для корректной работы приложения необходимо дать разрешения на камеру и харнилище данных!")
+                .setCancelable(false)
+                .setNegativeButton("ОК", (dialog, id) -> {
+                    dialog.cancel();
+                    requestPermissions();
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void requestPermissions() {
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        ).subscribe(granted -> {
+            if (granted) {
+                openOpeningLaunchScreen();
+            } else {
+                showDialog();
+            }
+        }, Throwable::printStackTrace);
+    }
+
 }
