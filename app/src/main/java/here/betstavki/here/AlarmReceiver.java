45a2dafa16f39193541544b94e5b4841e9f1b067
Alex Mihailov <avmikhaylov@tectus-it.ru>
2018-05-18 20:16:30 +0300

    Изменил текст пуша

--- app/src/main/java/here/betstavki/here/AlarmReceiver.java
+++ app/src/main/java/here/betstavki/here/AlarmReceiver.java
@@ -17,8 +17,8 @@ public class AlarmReceiver extends BroadcastReceiver {
         }
 
         NotificationScheduler.showNotification(context, SplashScreenActvity.class,
-                "Новые МАТЧИ уже тут⏱",
-                "Высокие коэффициенты, быстрые выплаты по новым матчам!");
+                "Новые СТАВКИ с высоким КОЭФФИЦИЕНТОМ⚡️",
+                "Новые турниры, новые ставки, спеши и выигрывай!");
 
     }
 }
